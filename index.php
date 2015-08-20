2997c985061899542b6dff31624452f978a95eb9
Terrance Woest <terrance@tremli.com>
2015-08-19 19:43:33 -0700

    Added bootstraps meta header for viewports so mobile looks better.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Citizenship Test : Flash Cards</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<style>
 	.answers {
